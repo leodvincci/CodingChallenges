dd4c8508ace7a684598e751e4b5e886f24b15797
leodvincci <lpenrose@depaul.edu>
2023-02-08 09:44:49 -0600

    completed even or odd

---
+++ CodeWars/Feb/even_or_odd.py
@@ -0,0 +1,6 @@
+#Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+#https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+
+def even_odd(num):
+    return "Even" if num % 2 == 0 else "Odd"
+
